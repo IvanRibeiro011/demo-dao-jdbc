ce7a64a1a6f9372f2ffd925296ceb16c961a997c
Ivanildo Ribeiro <ivanildo.ribeiro@arphoenix.com.br>
2023-04-20 14:46:42 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,11 @@
 package model.dao;
 
+import db.DB;
+import model.dao.impl.SellerDaoImplJdbc;
+
 public class DaoFactory {
 
+	public static SellerDao CreateSellerDao() {
+		return new SellerDaoImplJdbc(DB.getConnection());
+	}
 }
